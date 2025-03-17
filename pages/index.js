74b0d435cec06d32cde30c10dfa4942da871c25c
icm681 <icmdarkweb@gmail.com>
2025-03-17 22:18:55 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-// pages/index.js
 import ChatBox from "../components/ChatBox";
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Hayase AI</h1>
+      <h1>Hayase AI</h1>
       <ChatBox />
     </div>
   );
