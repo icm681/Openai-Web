ff61b9346c37143125afe0c1186afb854de71590
icm681 <icmdarkweb@gmail.com>
2025-03-17 22:13:37 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// pages/index.js
+import ChatBox from "../components/ChatBox";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Hayase AI</h1>
+      <ChatBox />
+    </div>
+  );
+}
