ad8f5a31c4c161b70fef8c56b500e96cb0427dab
icm681 <icmdarkweb@gmail.com>
2025-03-17 23:45:42 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: 'standalone',
-  env: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  },
 };
 
 module.exports = nextConfig;
