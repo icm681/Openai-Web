64cf8383724305b1a50571465adbd4c59e2ce335
icm681 <icmdarkweb@gmail.com>
2025-03-18 11:55:28 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: "standalone",
-  images: {
-    domains: ["your-image-domain.com"], // Sesuaikan kalau ada penggunaan Next.js Image
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: __dirname,
   },
 };
 
