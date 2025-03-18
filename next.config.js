3877eb71be5e4a24b052217fc6d1fc74b4a929a1
icm681 <icmdarkweb@gmail.com>
2025-03-18 10:17:12 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   output: 'standalone',
+  trailingSlash: true,
+  experimental: {
+    appDir: true, // Jika pakai Next.js 14 ke atas
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false }; // Hindari masalah dengan fs di serverless
+    return config;
+  },
 };
 
 module.exports = nextConfig;
