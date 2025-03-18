c8a0988c0bb51c156974b668d36ba89b03d0d578
icm681 <icmdarkweb@gmail.com>
2025-03-18 15:31:29 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   output: 'standalone',
   outputFileTracingRoot: __dirname,
+  api: {
+    bodyParser: true, // Memastikan API bisa menerima request body
+    externalResolver: true, // Menghindari Next.js menganggap API tidak digunakan
+  },
 };
 
 module.exports = nextConfig;
